682eb462695cbdfa313b4ce80a164f6fffbba29b
shruthii5666 <230701312@rajalakshmi.edu.in>
2024-11-09 22:56:47 +0530

    Add files via upload

---
+++ emp.sql
@@ -0,0 +1,6 @@
+select* from login;
+create table login(ID varchar(25), PW varchar(25));
+use hospital_management_system;
+create database hospital_management_system;
+INSERT INTO login VALUES ('abc', '123'), ('xyz', '789');
+select * from emp;
